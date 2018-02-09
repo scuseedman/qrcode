5a4bf2804559b228e1cedf861cf3f8ebeed892b1
seed <liwangdong@jrq.com>
2018-02-09 16:20:22 +0800

    test commit git !!

---
+++ src/main/java/com/seed/TestCommitGit.java
@@ -0,0 +1,66 @@
+/** 
+ * Project Name:QrCode 
+ * File Name:TestCommitGit.java 
+ * Package Name:com.seed 
+ * Date:2018年2月9日下午4:19:55 
+ * Copyright (c) 2018,  All Rights Reserved. 
+ * 
+* 　　　　　　　　┏┓　　　┏┓
+ * 　　　　　　　┏┛┻━━━┛┻┓
+ * 　　　　　　　┃　　　　　　　┃ 　
+ * 　　　　　　　┃　　　━　　　┃
+ * 　　　　　　　┃　＞　　　＜　┃
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃...　⌒　... ┃
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃　　　　　　　　　　
+ * 　　　　　　　　　┃　　　┃   神兽无影，BUG无踪！
+ * 　　　　　　　　　┃　　　┃　　　　　　　　　　　
+ * 　　　　　　　　　┃　　　┃  　　　　　　
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　　　　　　　　　　
+ * 　　　　　　　　　┃　　　┗━━━┓
+ * 　　　　　　　　　┃　　　　　　　┣┓
+ * 　　　　　　　　　┃　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛
+*
+*
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽无影，BUG无踪！
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ *
+ * ━━━━━━感觉萌萌哒━━━━━━
+*/ 
+package com.seed;
+/** 
+ * ClassName:TestCommitGit
+ * Date:     2018年2月9日 下午4:19:55 
+ * DESC:	
+ * @author   lwd 
+ * @version   
+ * @since    JDK 1.7
+ * @see       
+ */
+public class TestCommitGit {
+	public static void main(String[] args) {
+		System.out.println(" ==========>>>>>>");
+	}
+}
